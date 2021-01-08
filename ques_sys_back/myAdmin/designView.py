70b304c65f0339b3f67ac3d42396d894b53ca21a
zlf1123 <50043700+zlf1123@users.noreply.github.com>
2021-01-08 19:18:23 +0800

    Add files via upload

--- ques_sys_back/myAdmin/designView.py
+++ ques_sys_back/myAdmin/designView.py
@@ -3,8 +3,11 @@ import json
 from myAdmin.models import *
 from django.db import transaction,connection
 from django.db.models import Q
+from . import handle
 from io import BytesIO
 import base64
+
+
 ############################################################
 #功能：问卷设计者操作主入口
 #最后更新：2019-05-23
@@ -37,7 +40,7 @@ def opera(request):
                     response = resetpass(info)
                 elif opera_type == 'get_wj_list':  # 获取问卷列表
                     response = getWjList(info, username)
-                elif opera_type == 'search_user':  #搜索用户
+                elif opera_type == 'search_user':
                     response = searchuser(info, username)
                 elif opera_type == 'get_users_list':  # 获取用户列表
                     response = getUsersList(info, username)
@@ -45,12 +48,39 @@ def opera(request):
                     response = banwj(info, username)
                 elif opera_type == 'delete_wj':  # 删除问卷
                     response = deleteWj(info, username)
-                elif opera_type == 'exit':
-                    response = exit(request)
+                elif username:#需要验证username的方法
 
-                else:
-                    response['code'] = '-7'
-                    response['msg'] = '请求类型有误'
+                    if opera_type == 'add_wj':  # 添加问卷
+                        response = addWj(info,username)
+
+                    elif opera_type == 'get_temp_wj_list':  # 获取问卷列表
+                        response = getTempWjList(info,username)
+
+                    elif opera_type == 'get_question_list':  # 获取问题列表
+                        response = getQuestionList(info,username)
+                    elif opera_type == 'add_question':  # 添加问题
+                        response = addQuestion(info,username)
+                    elif opera_type == 'delete_question':  # 删除问题
+                        response = deleteQuestion(info,username)
+                    elif opera_type == 'push_wj':  # 发布问卷（更改问卷状态）
+                        response = pushWj(info,username)
+                    elif opera_type == 'dataAnalysis':#获取统计数据
+                        response = dataAnalysis(info)
+                    elif opera_type == 'add_temp':#添加模板
+                        response = addTemp(info,username)
+                    elif opera_type == 'use_temp':#添加模板
+                        response = useTemp(info,username)
+                    elif opera_type == 'exit':
+                        response = exit(request)
+                    elif opera_type=='get_text_answer_detail':
+                        response=getTextAnswerDetail(info)
+                    elif opera_type=='analysis_export_excel':
+                        response=analysisExportExcel(info)
+                    elif opera_type=='answer_text_to_excel':
+                        response=answerText2Excel(info)
+                    else:
+                        response['code'] = '-7'
+                        response['msg'] = '请求类型有误'
             else:
                 response['code'] = '-3'
                 response['msg'] = '确少必要参数'
@@ -77,41 +107,42 @@ def loginCheck(request):
             response['msg'] = '未登录'
     return response
 
-def getUsersList(info,username):
+
+
+############################################################
+#功能：添加问卷/更新问卷
+#最后更新：2019-05-23
+#备注：当传入id(问卷id)为空时：添加问卷  不为空时：更新问卷
+############################################################
+def addWj(info,username):
     response = {'code': 0, 'msg': 'success'}
-    if username != '':
-        obj = User.objects.all().order_by('username')
-        detail = []
-        for item in obj:
-            temp = {}
-            temp['username'] = item.username
-            temp['email'] = item.email
-            temp['status'] = item.status
-            detail.append(temp)
-        response['data'] = {'detail': detail}
+    title = info.get('title')#问卷标题
+    desc=info.get('desc')#问卷描述
+    id=info.get('id')#问卷id 可为空
+    if username and title:
+        try:
+            if id:#id不为空 更新问卷
+                res=Wj.objects.get(username=username,id=id)
+                res.title=title
+                res.desc=desc
+                res.save()
+            else:#否则 添加问卷
+                res = Wj.objects.create(username=username, title=title,desc=desc, status=0)
+        except:
+            response['code'] = '-4'
+            response['msg'] = '操作失败'
+        else:
+            if res.id > 0:
+                response['id'] = res.id
+            else:
+                response['code'] = '-4'
+                response['msg'] = '操作失败'
     else:
         response['code'] = '-3'
         response['msg'] = '确少必要参数'
     return response
 
-def searchuser(info,username):
-    response = {'code': 0, 'msg': 'success'}
-    username = info.get('username')
-    print(username)
-    if username != '':
-        obj = User.objects.filter(username=username)
-        detail = []
-        for item in obj:
-            temp = {}
-            temp['username'] = item.username
-            temp['email'] = item.email
-            temp['status'] = item.status
-            detail.append(temp)
-        response['data'] = {'detail': detail}
-    else:
-        response['code'] = '-3'
-        response['msg'] = '确少必要参数'
-    return response
+
 
 ############################################################
 #功能：获取问卷列表
@@ -137,6 +168,76 @@ def getWjList(info,username):
         response['code'] = '-3'
         response['msg'] = '确少必要参数'
     return response
+
+def getUsersList(info,username):
+    response = {'code': 0, 'msg': 'success'}
+    if username != '':
+        obj = User.objects.all().order_by('username')
+        detail = []
+        for item in obj:
+            temp = {}
+            temp['username'] = item.username
+            temp['email'] = item.email
+            temp['status'] = item.status
+            detail.append(temp)
+
+        response['data'] = {'detail': detail}
+
+    else:
+        response['code'] = '-3'
+        response['msg'] = '确少必要参数'
+    return response
+
+def searchuser(info,username):
+    response = {'code': 0, 'msg': 'success'}
+    username = info.get('username')
+    print(username)
+    if username != '':
+        obj = User.objects.filter(username=username)
+        detail = []
+        for item in obj:
+            temp = {}
+            temp['username'] = item.username
+            temp['email'] = item.email
+            temp['status'] = item.status
+            detail.append(temp)
+
+        response['data'] = {'detail': detail}
+
+    else:
+        response['code'] = '-3'
+        response['msg'] = '确少必要参数'
+    return response
+############################################################
+#功能：获取模板库问卷列表
+#最后更新：2019-06-16
+############################################################
+def getTempWjList(info,username):
+    response = {'code': 0, 'msg': 'success'}
+    page = info.get('page',1)  # 问卷标题
+    if username:
+        obj = TempWj.objects.all().order_by('id')
+        count=obj.count()
+        obj=obj[(page - 1) * 5: (page - 1) * 5 + 5]
+        detail=[]
+        for item in obj:
+            temp={}
+            temp['tempid']=item.id
+            temp['tempname']=item.title
+            temp['username'] = item.username
+            # temp['desc']=item.desc
+            detail.append(temp)
+        response['detail']=detail
+        response['count']=count
+
+    else:
+        response['code'] = '-3'
+        response['msg'] = '确少必要参数'
+    return response
+
+
+
+
 ############################################################
 #功能：删除问卷
 #最后更新：2019-05-23
@@ -178,6 +279,8 @@ def banwj(info,username):
         response['code'] = '-4'
         response['msg'] = '操作失败'
     return response
+
+
 ############################################################
 #功能：获取问题列表
 #最后更新：2019-05-27
@@ -218,6 +321,69 @@ def getQuestionList(info,username):
     return response
 
 
+
+
+
+############################################################
+#功能：添加问题/更新问题
+#最后更新：2019-05-23
+#备注：当传入questionId(问题id)为空时：添加问题  不为空时：更新问题
+############################################################
+#事务处理  当一次插入选项过多时（测试10个选项要5秒以上）很费时间 增加事务处理可大大加快速度（改进后20个选项3秒插入完成）
+@transaction.atomic
+def addQuestion(info,username):
+    response = {'code': 0, 'msg': 'success'}
+    wjId=info.get('wjId')#wjid
+    q_title=info.get('title')#题目标题
+    q_type=info.get('type')#题目类型
+    options=info.get('options')#选项
+    row=info.get('row')
+    must=info.get('must')
+    questionId=info.get('questionId')#问题id 可为空
+    if wjId and q_title and q_type and must!=None:
+        if q_type in ['radio','checkbox','text']:
+            if questionId:#问题id存在 更新问题
+                newIds=[]
+                for temp in options:
+                    newIds.append(temp['id'])#将更新后的选项id记录
+                allOptions=Options.objects.filter(questionId=questionId)
+                #遍历选项 把不在更新后的选项id中的选项删除
+                for option in allOptions:
+                    if option.id not in newIds:
+                        option.delete()
+                #更新问题
+                Question.objects.filter(wjId=wjId,id=questionId).update(title=q_title,type=q_type,must=must,row=row)
+                #更新选项
+                for option in options:
+                    if option['id']!=0:#选项为已有的 更新
+                        Options.objects.filter(questionId=questionId, id=option['id']).update(title=option['title'])
+                    else:#选项为新增的 添加
+                        Options.objects.create(questionId=questionId,title=option['title'])
+            else:#问题id不存在 添加问题
+                # 添加问题
+                resObj = Question.objects.create(wjId=wjId, title=q_title, type=q_type, row=row,must=must)
+                questionId = resObj.id
+                response['id'] = questionId
+                # 添加选项
+                if q_type == 'radio' or q_type == 'checkbox':  # 单选或者多选
+                    print(type(options))
+                    if options and type(options) == type([]):
+                        for item in options:
+                            Options.objects.create(questionId=questionId, title=item['title'])
+                            # Options(questionId=questionId,title=item)
+                    else:  # 传入选项不能为空
+                        response['code'] = '-4'
+                        response['msg'] = '操作失败'
+        else:
+            response['code'] = '-5'
+            response['msg'] = '传入参数值有误'
+            return response
+    else:
+        response['code'] = '-3'
+        response['msg'] = '确少必要参数'
+    return response
+
+
 ############################################################
 #功能：删除问题
 #最后更新：2019-05-23
@@ -243,6 +409,32 @@ def deleteQuestion(info,username):
         response['code'] = '-3'
         response['msg'] = '确少必要参数'
     return response
+
+
+
+############################################################
+#功能：发布问卷
+#最后更新：2019-05-24
+############################################################
+def pushWj(info,username):
+    response = {'code': 0, 'msg': 'success'}
+    wjId=info.get('wjId')
+    status=info.get('status')#0暂停问卷 1发布问卷
+    print('%s,%s,%s'%(wjId,username,status))
+    if wjId and username and (status==0 or status==1):
+        res=Wj.objects.filter(id=wjId,username=username)
+        if res.exists():  # 该题目是此用户创建的 有权限
+            res.update(status=status)
+        else:  # 该题目不是此用户创建的 无权限
+            response['code'] = '-6'
+            response['msg'] = '权限不足'
+    else:
+        response['code'] = '-3'
+        response['msg'] = '确少必要参数'
+
+    return response
+
+
 
 
 ############################################################
@@ -371,4 +563,263 @@ def resetpass(info):
     else:
         response['code'] = '-3'
         response['msg'] = '确少必要参数'
+    return response
+
+
+############################################################
+#功能：数据分析
+#最后更新：2019-05-28
+############################################################
+def dataAnalysis(info):
+    response = {'code': 0, 'msg': 'success'}
+    try:
+        wjId=info.get('wjId')#获取问卷id
+    except:
+        response['code'] = '-4'
+        response['msg'] = '操作失败'
+    else:
+        if wjId:  # 如果问卷id 存在
+            detail = []
+            questions = Question.objects.filter(wjId=wjId)
+            for question in questions:
+                questionTitle = question.title
+                questionType = question.type
+                questionId=question.id
+                if questionType == "radio" or questionType == "checkbox":
+                    result = getQuestionAnalysis(question.id)
+                    print(result)
+                else:
+                    # result = getQuestionText(question.id)
+                    result=''
+
+                detail.append({
+                    "title": questionTitle,
+                    "type": questionType,
+                    "result": result,
+                    "questionId":questionId
+                })
+            response['detail'] = detail
+        else:
+            response['code'] = '-3'
+            response['msg'] = '确少必要参数'
+    return response
+
+
+#根据问题id获取统计情况
+def getQuestionAnalysis(questionId):
+    # options=Options.objects.filter(questionId=questionId)   #获取问题的选项
+    # answer = Answer.objects.filter(questionId=questionId)  # 获取问题id的答案
+    # total = answer.count()  # 获取答案的总数
+    # result=[]
+    # for option in options:
+    #     optionTitle=option.title#获取问题选项
+    #     optionCount=Answer.objects.filter(questionId=questionId,answer=option.id).count()#获取每个选项数量
+    #     if total==0:
+    #         percent=0
+    #     else:
+    #         percent=int((optionCount/total)*10000)/100#获取每个选项的占比
+    #     result.append({
+    #         "option":optionTitle,
+    #         "count":optionCount,
+    #         "percent":str(percent)+'%'
+    #     })
+    # return result
+
+    result=[]
+    cursor=connection.cursor()
+    cursor.execute('select A.id,count(B.submitId),A.title from (select * from myAdmin_options where questionId=%s) A left join myAdmin_answer B on A.id=B.answer group by A.id'%questionId)
+    rows=cursor.fetchall()
+    total=0
+    for id,count,title in rows:
+        total+=count
+    for id,count,title in rows:
+        if total==0:
+            percent=0
+        else:
+            percent=int((count/total)*10000)/100
+        result.append({
+            'option':title,
+            'count':count,
+            'percent':str(percent)+'%'
+        })
+    print('questionId=',questionId)
+    print('result=',result)
+    return result
+
+#获取文本内容
+def getQuestionText(questionId):
+    answer = Answer.objects.filter(questionId=questionId)# 获取问题id的答案
+    result=[]
+    for item in answer:
+        if(item.answerText):
+            result.append({'context': item.answerText})
+    return result
+
+
+#获取文本回答详情
+def getTextAnswerDetail(info):
+    response = {'code': 0, 'msg': 'success'}
+    questionId=info.get('questionId')
+    pageSize=info.get('pageSize',10)
+    currentPage=info.get('currentPage',1)
+    if not questionId:
+        response['code'] = '-4'
+        response['msg'] = '操作失败'
+        return response
+    answer=Answer.objects.filter(~Q(answerText=''),questionId=questionId,answerText__isnull=False)
+    total=answer.count()
+    answer=answer[(currentPage-1)*pageSize:currentPage*pageSize]
+    result = []
+    for item in answer:
+
+        result.append({'context': item.answerText})
+    response['detail']=result
+    response['total']=total
+    return response
+
+
+
+############################################################
+#功能：添加模板（临时）
+#说明：根据wjId，将现有的问卷添加到模板库
+#最后更新：2019-06-16
+############################################################
+def addTemp(info,username):
+    response = {'code': 0, 'msg': 'success'}
+    wjId=info.get('wjId')
+    if wjId:
+        try:
+            wjItem=Wj.objects.get(id=wjId)
+            # 添加问卷信息
+            wjRes=TempWj.objects.create(
+                title=wjItem.title,
+                username=wjItem.username,
+                desc=wjItem.desc
+            )
+            #添加问题信息
+            questions=Question.objects.filter(wjId=wjId)
+            for q in questions:
+                qRes=TempQuestion.objects.create(
+                    title=q.title,
+                    type=q.type,
+                    wjId=wjRes.id,
+                    row=q.row,
+                    must=q.must
+                )
+                #添加选项
+                options=Options.objects.filter(questionId=q.id)
+                for o in options:
+                    TempOptions.objects.create(
+                        questionId=qRes.id,
+                        title=o.title
+                    )
+
+        except:
+            response['code'] = '-4'
+            response['msg'] = '操作失败'
+
+    else:
+        response['code'] = '-3'
+        response['msg'] = '确少必要参数'
+
+    return response
+
+
+
+############################################################
+#功能：使用模板
+#说明：根据模板wjId，将模板库问卷添加到用户问卷
+#最后更新：2019-06-16
+############################################################
+def useTemp(info,username):
+    response = {'code': 0, 'msg': 'success'}
+    wjId=info.get('wjId')
+    if wjId:
+        try:
+            wjItem=TempWj.objects.get(id=wjId)
+            # 添加问卷信息
+            wjRes=Wj.objects.create(
+                title=wjItem.title,
+                username=username,
+                desc=wjItem.desc,
+                status=0
+            )
+            #添加问题信息
+            questions=TempQuestion.objects.filter(wjId=wjId)
+            for q in questions:
+                qRes=Question.objects.create(
+                    title=q.title,
+                    type=q.type,
+                    wjId=wjRes.id,
+                    row=q.row,
+                    must=q.must
+                )
+                #添加选项
+                options=TempOptions.objects.filter(questionId=q.id)
+                for o in options:
+                    Options.objects.create(
+                        questionId=qRes.id,
+                        title=o.title
+                    )
+
+        except:
+            response['code'] = '-4'
+            response['msg'] = '操作失败'
+
+    else:
+        response['code'] = '-3'
+        response['msg'] = '确少必要参数'
+
+    return response
+
+
+# 导出excel
+def analysisExportExcel(info):
+    response = {'code': 0, 'msg': 'success'}
+    wjId=info.get('wjId')
+    if not wjId:
+        response['code'] = '-3'
+        response['msg'] = '确少必要参数'
+        return response
+    wj=Wj.objects.get(id=wjId)
+    title=wj.title
+    data=dataAnalysis(info)
+    if data['code']==0:
+        detail=data['detail']
+        wb=handle.analysisExportExcel(detail,title)
+        bio = BytesIO()
+        wb.save(bio)
+        bio.seek(0)
+        response['filename']='%s.xls'%title
+        response['b64data']=base64.b64encode(bio.getvalue()).decode()
+        return response
+    else:
+        return response
+
+#回答文本导出excel
+def answerText2Excel(info):
+    response = {'code': 0, 'msg': 'success'}
+    questionId=info.get('questionId')
+    if not questionId:
+        response['code'] = '-3'
+        response['msg'] = '确少必要参数'
+        return response
+    try:
+        q = Question.objects.get(id=questionId)
+    except:
+        response['code']='-4'
+        response['code']='操作失败'
+        return response
+    else:
+        title=q.title
+    answer = Answer.objects.filter(~Q(answerText=''), questionId=questionId, answerText__isnull=False)
+    data=[]
+    for item in answer:
+        data.append(item.answerText)
+    wb=handle.answerText2Excel(data)
+    bio = BytesIO()
+    wb.save(bio)
+    bio.seek(0)
+    response['filename'] = '%s.xls' % title
+    response['b64data'] = base64.b64encode(bio.getvalue()).decode()
     return response
